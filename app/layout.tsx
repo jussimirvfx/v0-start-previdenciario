a79e480674b43c56bf4ed6ed8ff6f7302137b3ee
jussimirvfx <jussimir@agenciavfx.com.br>
2025-04-17 11:35:07 -0300

    feat: configura Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next"
 import { Montserrat } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Bubble } from "@typebot.io/nextjs"
-import { GoogleTagManager } from "@/components/GoogleTagManager"
+import Script from "next/script"
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -25,8 +25,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className="scroll-smooth">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TJ2BVCWX');`}
+      </Script>
       <body className={`${montserrat.variable} font-sans bg-black`}>
-        <GoogleTagManager />
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJ2BVCWX"
+        height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
           {children}
           <Bubble
